62868881983cead870d769c51363b17dd88d0139
CoreFloDev <CoreFloDev@users.noreply.github.com>
2017-01-05 22:31:25 +0000

    Add test to common package

---
+++ app/src/test/java/io/coreflodev/httpcacheexample/common/dagger/ApplicationModuleTest.java
@@ -0,0 +1,41 @@
+package io.coreflodev.httpcacheexample.common.dagger;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.coreflodev.httpcacheexample.api.ChatService;
+import io.coreflodev.httpcacheexample.common.network.NetworkService;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ApplicationModuleTest {
+
+    @Mock
+    NetworkService networkServiceMock;
+    @Mock
+    ChatService chatServiceMock;
+
+    private ApplicationModule applicationModule;
+
+    @Before
+    public void setup() {
+        applicationModule = new ApplicationModule();
+    }
+
+    @Test
+    public void testProvideNetworkService() {
+        assertNotNull(applicationModule.provideNetworkService());
+    }
+
+    @Test
+    public void testProvideChatServices() {
+        when(networkServiceMock.createService(ChatService.class)).thenReturn(chatServiceMock);
+        assertEquals(chatServiceMock, applicationModule.provideChatService(networkServiceMock));
+    }
+}
